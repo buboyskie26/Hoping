f156a4c1e1cff48de99ae67d0097ea7ee171761d
Sirios <Sirios@DESKTOP-P8365NO>
2021-10-27 15:20:14 +0800

    Add project files.

---
+++ WebGuide/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebGuide.Data;
+
+namespace WebGuide.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public BooksController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+      
+    }
+}
